5b76dcd1c87353d4bc93f299f2567264b189c519
CRBroughton <crbroughton@posteo.uk>
2022-06-18 20:01:13 +0100

    feat(socket.io): create basic socket.io connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 /* eslint-disable no-console */
 import http from 'http'
 import express from 'express'
+import { Server } from 'socket.io'
 const app = express()
 const server = http.createServer(app)
+const io = new Server(server)
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>')
+io.on('connection', (_socket) => {
+  console.log('a user connected')
 })
 
 server.listen(3000, () => {
